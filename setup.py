127c28029107c5d7d5fc02d0b6c1bfc92b4cab9b
laleong <laleong@uw.edu>
2023-12-16 23:11:51 -0800

    more testing

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="more_metrics",  
+    version="0.9.3", 
+    author="Lambert T Leong",  
+    author_email="lamberttleong@gmail.com",  
+    description="A Python package for computing additional classification metrics for machine learning or AI models. These metrics include and also known as AUC, NRI, and IDI",  
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/LambertLeong/AUC_NRI_IDI_python_functions", 
+    license="GPL-3.0",  
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+	'matplotlib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Minimum version requirement of the Python for your package
+)
+
